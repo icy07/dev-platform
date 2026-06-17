fd19f87f6572fdf013e28a4bd353d9f210b27c4f
Andrey Ilin <andrey.ilin823@gmail.com>
2026-06-17 17:52:08 +0300

    feat: added image upload to the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRoutes from "./routes/auth";
 import postRoutes from "./routes/posts";
+import path from "path";
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use(
 
 app.use("/api", authRoutes);
 app.use("/api", postRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 mongoose
 	.connect(process.env.MONGODB_URI!)
