f6461eae5dc19fe212fa237514152a080afc934b
Andrey Ilin <andrey.ilin823@gmail.com>
2026-06-24 22:18:07 +0300

    feat: added projectcard component and stylized profilepage

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "../../store/store";
 import PostsMain from "../../components/PostsMain/PostsMain";
-import PostModal from "../../components/PostModal/PostModal";
 import { getPostsRequest } from "../../api/postsApi";
 import { setPosts } from "../../store/slices/postsSlice";
 import Loader from "../../components/Loader/Loader";
--- client/src/pages/ProfilePage/ProfilePage.tsx
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -6,12 +6,15 @@ import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "../../store/store";
 import { setProfile } from "../../store/slices/profileSlice";
 import Loader from "../../components/Loader/Loader";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
 
 const ProfilePage = () => {
 	const { id } = useParams();
 
 	const dispatch = useDispatch<AppDispatch>();
 	const profile = useSelector((state: RootState) => state.profile.profile);
+	const { user } = useSelector((state: RootState) => state.auth);
+	const [isOwner, setIsOwner] = useState(false);
 
 	const [loading, setLoading] = useState<boolean>(false);
 
@@ -30,6 +33,12 @@ const ProfilePage = () => {
 		fetchProfile();
 	}, [id]);
 
+	useEffect(() => {
+		if (user && profile) {
+			setIsOwner(user.id === profile._id);
+		}
+	}, [user, profile]);
+
 	return (
 		<>
 			{loading && (
@@ -39,11 +48,85 @@ const ProfilePage = () => {
 			)}
 			{profile && (
 				<div className={styles.profile}>
-					<div className={styles.profile__info}>
-						<h1>{profile.nickname}</h1>
-						<p>{profile.firstName}</p>
-						<p>{profile.lastName}</p>
+					<div className={styles.profile__wrapper}>
+						<div className={styles.profile__top}>
+							<h2 className={styles.profile__title}>Информация</h2>
+							{isOwner && (
+								<button className={styles.profile__btn}>
+									<svg
+										width="15"
+										height="15"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										strokeWidth="1.5"
+										strokeLinecap="round"
+									>
+										<path d="M4 20h4l10.5-10.5a2.828 2.828 0 1 0-4-4L4 16v4" />
+									</svg>
+									Редактировать
+								</button>
+							)}
+						</div>
+						<div className={styles.profile__info}>
+							<p>
+								Имя: <span>{profile.firstName}</span>
+							</p>
+							<p>
+								Фамилия: <span>{profile.lastName}</span>
+							</p>
+							<p>
+								Никнейм: <span>{profile.nickname}</span>
+							</p>
+							<p>
+								Роль: <span>{profile.role}</span>
+							</p>
+							{profile.workplace && (
+								<p>
+									Место работы: <span>{profile.workplace}</span>
+								</p>
+							)}
+							{profile.description && (
+								<p>
+									Описание: <span>{profile.description}</span>
+								</p>
+							)}
+						</div>
 					</div>
+					{profile.portfolio.length > 0 && (
+						<div className={styles.profile__wrapper}>
+							<div className={styles.profile__top}>
+								<h2 className={styles.profile__title}>Портфолио</h2>
+								{isOwner && (
+									<button className={styles.profile__btn}>
+										<svg
+											width="15"
+											height="15"
+											viewBox="0 0 24 24"
+											fill="none"
+											stroke="currentColor"
+											strokeWidth="1.5"
+											strokeLinecap="round"
+										>
+											<path d="M4 20h4l10.5-10.5a2.828 2.828 0 1 0-4-4L4 16v4" />
+										</svg>
+										Добавить проект
+									</button>
+								)}
+							</div>
+							<div className={styles.profile__projects}>
+								{profile.portfolio.map((project) => (
+									<ProjectCard
+										project={project}
+										isOwner={isOwner}
+										key={project._id}
+										onEdit={() => console.log("edit")}
+										onDelete={() => console.log("del")}
+									/>
+								))}
+							</div>
+						</div>
+					)}
 				</div>
 			)}
 		</>
